3152b0387d54b232bea9c60cfbd30b32a853ddf4
Dika Elsaputra <147382029+figuran04@users.noreply.github.com>
2024-05-30 17:04:02 +0700

    Update index.jsx

--- src/components/ListBerita/index.jsx
+++ src/components/ListBerita/index.jsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const ListBerita = ({ title, url }) => {
   return (
-    <div className="flex flex-wrap my-2 justify-center border-b-2 border-gray-500">
-      <Image width={330} height={100} src={url} alt={url} classname="rounded-md" />
+    <div className="flex flex-wrap my-2 justify-center border-b-2 border-gray-100">
+      <Image width={330} height={100} src={url} alt={url} classname="rounded-xl" />
       <div className="py-2">
         <h1 className="text-xl font-semibold line-clamp-3">{title}</h1>
       </div>
