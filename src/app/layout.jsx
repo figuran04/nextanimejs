936e855058607d3bd20869526cf890944ddaf18f
figuran04 <dikaelsaputra2406@gmail.com>
2024-06-01 23:02:26 +0700

    edit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google"
 import "./globals.css"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -16,7 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
