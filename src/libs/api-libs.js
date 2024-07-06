a308e60f7fa7861812a5639ba464800fcf9cb6c0
figuran04 <dikaelsaputra2406@gmail.com>
2024-07-06 02:19:07 -0600

    simple

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -1,44 +1,27 @@
 export const getAnimeResponse = async (resource, query) => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
-    )
-    console.log("Respons API:", response)
-    if (!response.ok) {
-      throw new Error("Respons jaringan tidak oke")
-    }
-    const anime = await response.json()
-    console.log("Data anime:", anime)
-    return anime
-  } catch (error) {
-    console.error("Error mengambil data anime:", error)
-    return null
-  }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+  )
+
+  const anime = await response.json()
+
+  return anime
 }
 
 export const getNestedAnimeResponse = async (resource, objectProperty) => {
   const response = await getAnimeResponse(resource)
-  if (response && response.data) {
-    return response.data.flatMap((item) => item[objectProperty])
-  } else {
-    console.error("Error: Struktur respons tidak valid")
-    return []
-  }
+
+  return response.data.flatMap((item) => item[objectProperty])
 }
 
 export const reproduce = (data, gap) => {
-  if (data && data.data && Array.isArray(data.data)) {
-    const anime = data.data
-    const first = Math.floor(Math.random() * (anime.length - gap))
-    const last = first + gap
+  const anime = data.data
+  const first = Math.floor(Math.random() * (anime.length - gap))
+  const last = first + gap
 
-    const response = {
-      data: anime.slice(first, last),
-    }
-    console.log(response)
-    return response
-  } else {
-    console.error("Error: Struktur data tidak valid di fungsi reproduce")
-    return { data: [] }
+  const response = {
+    data: anime.slice(first, last),
   }
+
+  return response
 }
