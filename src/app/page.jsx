be8201f3af323efb673b76c72cc1d074df3188d5
figuran04 <dikaelsaputra2406@gmail.com>
2024-07-07 14:45:03 +0700

    mendatang list

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,10 @@ const HomePage = async () => {
     "seasons/upcoming",
     "limit=5"
   )
-  let recommendedAnime = await getAnimeResponse("recommendations/anime")
   // let recommendedAnime = await getNestedAnimeResponse("recommendations/anime")
-  recommendedAnime = reproduce(recommendedAnime, 4)
+
+  // let recommendedAnime = await getAnimeResponse("recommendations/anime")
+  // recommendedAnime = reproduce(recommendedAnime, 4)
   return (
     <div className="w-full flex flex-col items-center">
       <div className="w-11/12 flex md:flex-row flex-col gap-6">
