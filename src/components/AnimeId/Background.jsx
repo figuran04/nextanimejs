93cc56f4f4465d25d7440e2880ff9bd2615346bc
figuran04 <dikaelsaputra2406@gmail.com>
2024-07-28 14:13:43 +0700

    test: bground text

--- src/components/AnimeId/Background.jsx
+++ src/components/AnimeId/Background.jsx
@@ -3,7 +3,7 @@ const Background = ({ data }) => {
     <div className="my-2">
       <span className="text-xl font-bold">Latar Belakang</span>
       <div className="flex gap-2 overflow-x-auto">
-        <p>{data.background}</p>
+        <p className="text-justify">{data.background}</p>
       </div>
     </div>
   )
